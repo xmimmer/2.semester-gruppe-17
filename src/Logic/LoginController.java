1cf3177382119828d77772cc1c6172a4dfbbb928
Mimmer <maha618@student.sdu.dk>
2019-04-05 20:32:13 +0200

    improvement
    FXML dokumenterne er nu pænere, og der er rettet lidt forskelligt på profilerne.

--- src/Logic/LoginController.java
+++ src/Logic/LoginController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Logic;
 
 import java.io.IOException;
@@ -21,11 +17,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Mimmer
- */
+
 public class LoginController implements Initializable {
 
     @FXML
