eb68b830ba693510492f4cef0b9d43af317df5e5
Mimmer <maha618@student.sdu.dk>
2019-04-24 22:01:58 +0200

    Bedre funktionalitet med database.
    Nu undgår vi at oprette FXML dokumenter for hver enkelt borger.

--- src/Logic/AdminController.java
+++ src/Logic/AdminController.java
@@ -1,7 +1,5 @@
 package Logic;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -9,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -22,29 +19,53 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class AdminController implements Initializable {
 
     @FXML
     private ListView<String> borgerListView;
-    @FXML
-    private Label loggedInAsLabel;
-    @FXML
-    private Button backButton;
+
     @FXML
     private Button chooseButton;
     @FXML
     private Button logoutButton;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane startPane;
+    @FXML
+    private Pane borgerPane;
+    @FXML
+    private TextField depTextField;
+    @FXML
+    private TextField ageTextField;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private TextField cprTextField;
+    @FXML
+    private Button backButton1;
+    @FXML
+    private Label loggedInAsLabel1;
+    @FXML
+    private Label loggedInAsLabel2;
+    @FXML
+    private ImageView photo;
+    @FXML
+    private Label messageLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-        loggedInAsLabel.setText("Logget ind som: admin");
-
-        File file = new File("borgerliste.txt");
-        Scanner sc;
-
+        loggedInAsLabel1.setText("Logget ind som: admin");
+        loggedInAsLabel2.setText("Logget ind som: admin");
+        //File file = new File("borgerliste.txt");
+        // Scanner sc;
         ObservableList<String> list = FXCollections.observableArrayList();
 
         try {
@@ -62,47 +83,72 @@ public class AdminController implements Initializable {
             System.out.println("Connected!");
 
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM borgere");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM citizens");
             while (resultSet.next()) {
-                list.add(resultSet.getString("navn"));
+                list.add(resultSet.getString("name"));
 
             }
         } catch (java.sql.SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        list.add("Allan Nymark");
+
         borgerListView.setItems(list);
     }
 
     @FXML
-    private void backButtonHandler(ActionEvent event) throws IOException {
+    private void chooseButtonHandler(ActionEvent event) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/Presentation/LoginFXML.fxml"));
+        try {
+            String valgtBorger = borgerListView.getSelectionModel().getSelectedItem();
+            if (valgtBorger.trim().length() > 0) {
+
+                startPane.setVisible(false);
+                borgerPane.setVisible(true);
+
+                try {
+                    Class.forName("org.postgresql.Driver");
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Fail to load the jdbc driver.");
+                }
+
+                String databaseURL = "jdbc:postgresql://balarama.db.elephantsql.com:5432/beucjfoi";
+                String username = "beucjfoi";
+                String password = "EXQJyo9fmXNKkqC-CVoGoI2kE9XinAP8";
+
+                try {
+                    Connection connection = DriverManager.getConnection(databaseURL, username, password);
+                    System.out.println("Connected!");
+
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("SELECT name,age,department,CPR FROM citizens WHERE name ='" + valgtBorger + "'");
+                    while (resultSet.next()) {
+                        nameLabel.setText(resultSet.getString("name"));
+                        ageTextField.setText(resultSet.getString("age"));
+                        depTextField.setText(resultSet.getString("department"));
+                        cprTextField.setText(resultSet.getString("CPR"));
+
+                    }
+                } catch (java.sql.SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+            }
+        } catch (Exception e) {
+            messageLabel.setText("Ingen borger valgt.");
+        }
+
+    }
+
+    @FXML
+    private void backButton1Handler(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Presentation/AdminFXML.fxml"));
         Scene scene = new Scene(root);
 
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         window.setScene(scene);
         window.show();
-    }
-
-    @FXML
-    private void chooseButtonHandler(ActionEvent event) throws IOException {
-        try {
-            String valgtBorger = borgerListView.getSelectionModel().getSelectedItem();
-
-            Parent root = FXMLLoader.load(getClass().getResource("/Presentation/" + valgtBorger + ".fxml"));
-            Scene scene = new Scene(root);
-
-            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-            window.setScene(scene);
-            window.show();
-
-        } catch (Exception e) {
-            System.out.println("Ingen data tilgængelig for denne person.");
-        }
-
     }
 
     @FXML
